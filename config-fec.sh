0e35e9b2288820561b79d9537486be729b79fa4b
Tristan Smith <tas09914@gmail.co>
2020-08-20 00:57:56 -0400

    restyled

--- config-fec.sh
+++ config-fec.sh
@@ -5,7 +5,7 @@ PROXY=$1
 repos=($PROXY media-window purchase-options similar-games reviews game-sidebar game-description)
 
 for Dir in "${repos[@]}"; do
-  git clone http://github.com/hrr47-FEC-Bailey/$Dir
+  git clone http://github.com/hrr47-FEC-Bailey/$Dir/
   cd $Dir
   npm i
   cd ..
